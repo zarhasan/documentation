ca3d0f0ee1eed33786c5aa39e4611f38ecf14e64
Khizar Hasan <khizarhasan925@gmail.com>
2024-07-13 20:47:37 +0530

    update

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 require_once get_template_directory() . '/lib/class-tgm-plugin-activation.php';
 require_once get_template_directory() . '/lib/BreadcrumbsTrail.php';
 
-define('DOCUMENTATION_VERSION', '1.0.10');
+define('DOCUMENTATION_VERSION', '1.0.11');
 define('DOCUMENTATION_JOIN_SYMBOL', ' / ');
 define('DOCUMENTATION_CACHE_DIR',  WP_CONTENT_DIR . '/cache/documentation/');
 
